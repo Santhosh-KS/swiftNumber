389b758282a2ae87e2e8e73c7b6ad85c9110e6bc
Santhosh K S <santhoshachar08@gmail.com>
2020-09-30 21:37:22 +0200

    public init

--- Sources/swiftNumber/main.swift
+++ Sources/swiftNumber/main.swift
@@ -1,9 +1,11 @@
-import Foundation
-//import _Differentiation
+//import Foundation
 
 public struct myNumber<T: Numeric> {
     var num:T = .zero
+    public init(_ n:T) {
+        self.num = n
+    }
 }
 
-let v = myNumber(num:5)
+let v = myNumber.init(5)
 print(v.num)
--- Tests/swiftNumberTests/swiftNumberTests.swift
+++ Tests/swiftNumberTests/swiftNumberTests.swift
@@ -1,11 +1,9 @@
 import XCTest
-import class Foundation.Bundle
-
 @testable import swiftNumber
 
 final class swiftNumberTests: XCTestCase {
   func testNum() {
-    let v = myNumber(num:1)
+    let v = myNumber(1)
     XCTAssertEqual(1, v.num)
   }
 }
